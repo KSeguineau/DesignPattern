53aa65cf0aa91ea9e9f297e2ba0940e9c9a3f635
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-07-02 11:44:36 +0200

    initialisation

---
+++ src/main/java/Diginamic/DesignPattern/App.java
@@ -0,0 +1,13 @@
+package Diginamic.DesignPattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
